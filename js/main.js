d662636421e6d4c70c43a4355f3eed3e6242bca2
AzizZouaghia <azizzouaghia360@gmail.com>
2022-08-23 16:53:46 +0100

    Huge Update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,42 @@
+/* Load Page Start */
+let Loader = document.getElementById("LoadPage");
+window.addEventListener("load",()=>{
+    Loader.style.display='none';
+})
+/* Load Page End */
+Mode();
+/* Mode Change Start */
+function Mode() {
+if(localStorage.getItem("Mode")==="Dark"){
+// Style Sheet
+document.getElementById("stylesheet").href = "./css/darkStyle.css";
+// Hello Btn
+document.querySelector(".hello-btn").classList.remove("btn-outline-primary");
+document.querySelector(".hello-btn").classList.add("btn-outline-dark");
+
+}else if (localStorage.getItem("Mode") === "Default") {
+// Style Sheet
+document.getElementById("stylesheet").href = "./css/defaultStyle.css";
+// Hello Btn
+document.querySelector(".hello-btn").classList.remove("btn-outline-dark");
+document.querySelector(".hello-btn").classList.add("btn-outline-primary");
+
+}else{
+    localStorage.setItem("Mode", "Default");
+}
+}
+document.querySelector(".change-mode").onclick = ()=>{
+if(localStorage.getItem("Mode")==="Dark"){
+localStorage.setItem("Mode","Default");
+Mode();
+}else if(localStorage.getItem("Mode")==="Default") {
+localStorage.setItem("Mode", "Dark");
+Mode();
+}
+}
+
+/* Mode Change Ends */
+
 /* Fixed The Navbar Start */
 let navbar  = document.getElementById("navbar");
 let navlink = document.querySelectorAll(".nav-l");
@@ -19,7 +58,6 @@ window.addEventListener('scroll',()=>{
 /* Fix The UnderLine Start */
 let text = document.querySelectorAll(".projects .card h5");
 text.forEach((el)=>{
-    ;
     let width = el.clientWidth;
     if(screen.width <= 580) {
         left = (150-width)/2;
@@ -48,12 +86,38 @@ window.onscroll = ()=>{
         progressBar.forEach((e) => {
         e.style.width = e.dataset.width;
     })}
-    if (window.scrollY > lang.offsetTop - 400) {
+    if (window.scrollY > lang.offsetTop - 500 ) {
         progressBarLang.forEach((e) => {
         e.style.width = e.dataset.width;
     })}
 }
+/* Animation Ends */
 
-
-
-/* Animation Start */
+/* Writing Start :) */
+function writingAnimation(title,Stime,ScNum=0){ // Function
+let titleArray = title.textContent.split("");
+let rez = [titleArray[0]];
+title.innerHTML = titleArray[0];
+let i = 1;
+let WCheck = true;
+window.addEventListener("scroll", ()=>{
+    if (window.scrollY > title.offsetTop - ScNum && WCheck == true) {
+        WCheck = false;
+    let yo = setInterval(() => {
+        rez[i] = titleArray[i];
+        title.innerHTML = rez.join("");
+        i++;
+        i === titleArray.length ? clearInterval(yo) : false;
+    }, Stime);
+    }})};
+// Add Animation
+let title4 = document.querySelector(".hello .title");
+writingAnimation(title4, 150 , 580);
+let title1 = document.querySelector(".resume .title");
+writingAnimation(title1, 150 ,580 );
+let title2 = document.querySelector(".portfolio .title");
+writingAnimation(title2, 150 , 580);
+let title3 = document.querySelector(".contact .title"); 
+let LastTitleArg =  -(document.body.clientHeight) - (screen.width<= 580?180:-130) ; //Param For The Last Title
+writingAnimation(title3, 150,LastTitleArg);
+/* Writing End :)  */
